6a3e26bb62f3de02a92ff60bd727c2a419bb2443
Utku Bektasoglu <utkbktss5@gmail.com>
2024-01-23 20:54:48 +0300

    update route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
 import prisma from "@/libs/prismadb";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth,{ AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
 import { Adapter } from "next-auth/adapters";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
- export const authOptions: AuthOptions = {
+import { NextAuthOptions } from "next-auth";
+ export const authOptions: NextAuthOptions  = {
   adapter:PrismaAdapter(prisma) as Adapter,
   providers: [
     GithubProvider({
