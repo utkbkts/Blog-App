ff8b0b8ec29370760c9b02283059c7daeae33147
Utku Bektasoglu <utkbktss5@gmail.com>
2024-01-24 14:40:02 +0300

    update prisma

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,17 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+const client = globalThis.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default client
